6ccee7c3ed478a9a7270c2c7f5d546a4deec6c12
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-12-07 22:28:13 +0100

    Updated documentaion

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -3,12 +3,12 @@ import swaggerJSDoc from 'swagger-jsdoc';
 const swaggerDefinition = {
   openapi: '3.0.0',
   info: {
-    title: 'Draftbit test app REST API', // Title of the documentation
+    title: 'Authentication demo REST API', // Title of the documentation
     version: '1.0.0', // Version of the app
-    description: 'REST API for a draftbit app', // short description of the app
+    description: 'REST API for an authentication demo', // short description of the app
   },
   servers: [
-    { url: 'https://draftbit-app.herokuapp.com/api/v1', description: 'Deployed server on Heroku' },
+    { url: 'https://auth-demo-backend.herokuapp.com//api/v1', description: 'Deployed server on Heroku' },
     { url: 'http://localhost:3000/api/v1', description: 'Local development/testing server' },
   ],
   components: {
